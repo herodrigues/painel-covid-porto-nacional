e477c53588b7ea2775a129af4f0c3bfb18d54683
Herinson Rodrigues <herinson.br@gmail.com>
2020-05-20 09:29:59 -0300

    Run build

---
+++ precache-manifest.eb8c48770e66faaf3c8fdc749599add9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf4639fb83d7f3165c62782ccd66b36",
+    "url": "/painel-covid-porto-nacional/index.html"
+  },
+  {
+    "revision": "929389536d56e7b1ba28",
+    "url": "/painel-covid-porto-nacional/static/css/main.a4e5421f.chunk.css"
+  },
+  {
+    "revision": "843d0e7eac35c38a302e",
+    "url": "/painel-covid-porto-nacional/static/js/2.b6936775.chunk.js"
+  },
+  {
+    "revision": "7698ab7fef391e31a472e6dff7d9c09f",
+    "url": "/painel-covid-porto-nacional/static/js/2.b6936775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929389536d56e7b1ba28",
+    "url": "/painel-covid-porto-nacional/static/js/main.b99f826b.chunk.js"
+  },
+  {
+    "revision": "c308ab6c9c514468fbdb",
+    "url": "/painel-covid-porto-nacional/static/js/runtime-main.7433c586.js"
+  }
+]);
