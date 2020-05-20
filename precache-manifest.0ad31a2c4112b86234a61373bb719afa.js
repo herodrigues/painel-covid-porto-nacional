5e261504bfe0e239fe770e5f7f0a12e56aea13a7
Herinson Rodrigues <herinson.br@gmail.com>
2020-05-20 08:55:43 -0300

    Update homepage path

---
+++ precache-manifest.0ad31a2c4112b86234a61373bb719afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d87b4f0a2f575a3db915323d682468c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dfa281c0ac80ff693b3a",
+    "url": "./static/css/main.a4e5421f.chunk.css"
+  },
+  {
+    "revision": "947f30d8e1760b1abe17",
+    "url": "./static/js/2.45197be1.chunk.js"
+  },
+  {
+    "revision": "7698ab7fef391e31a472e6dff7d9c09f",
+    "url": "./static/js/2.45197be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa281c0ac80ff693b3a",
+    "url": "./static/js/main.5b68388e.chunk.js"
+  },
+  {
+    "revision": "6ec41cf04480d0c767a1",
+    "url": "./static/js/runtime-main.7a5de63f.js"
+  }
+]);
