43568a81c7beace15a625335ebbd519dea1575ee
Herinson Rodrigues <herinson.br@gmail.com>
2020-05-20 08:40:50 -0300

    Move build to master

---
+++ precache-manifest.b2767686bdb00c218d15dcdd0c2de8e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f57a8eeb33af67c00702ca1d17b25e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "372d2ccd673cfeaf051f",
+    "url": "/static/css/main.2c8eb123.chunk.css"
+  },
+  {
+    "revision": "07330d49669be860d03a",
+    "url": "/static/js/2.4e7f118c.chunk.js"
+  },
+  {
+    "revision": "7698ab7fef391e31a472e6dff7d9c09f",
+    "url": "/static/js/2.4e7f118c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372d2ccd673cfeaf051f",
+    "url": "/static/js/main.3c9b3c5b.chunk.js"
+  },
+  {
+    "revision": "475e629f97b572cbd433",
+    "url": "/static/js/runtime-main.82a2a924.js"
+  }
+]);
